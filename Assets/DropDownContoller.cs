0343939a52b16aa12a4e5347866c67a921ddef60
osmenog <mail@arod.in>
2021-01-04 22:21:00 +0300

    Test DropDown scripting

---
+++ Assets/DropDownContoller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDownContoller : MonoBehaviour {
+	public TextMesh textMesh;
+	// Use this for initialization
+	void Start () {
+        Dropdown drop = this.gameObject.GetComponent<Dropdown>();
+        Dropdown.OptionData optionData = new Dropdown.OptionData("ЕБАТЬ");
+		drop.AddOptions(new List<Dropdown.OptionData> { optionData });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
